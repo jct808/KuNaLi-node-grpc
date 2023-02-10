48a1c9c10e7581ac21a00d5cdf87519272de5988
jct808 <jct808@gmail.com>
2023-02-10 17:48:56 +0800

    KuNaLi-node-grpc Python Project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from api.dropbox import upload_file_to_dropbox
 from api.google_drive import upload_file_to_google_drive
 from api.onedrive import upload_file_to_one_drive
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # upload_file_to_google_drive("building.jpg")
-    upload_file_to_one_drive("building.jpg")
+    upload_file_to_dropbox("building.jpg")
 
